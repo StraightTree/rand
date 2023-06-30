9d1a6214c39d2a23d6a269959f6678da36d53f2a
David, Fruehwirth <david.fruehwirth@student.tugraz.at>
2023-06-30 08:45:37 +0200

    Modified main-function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "utility.hpp"
 #include "argument_parser.hpp"
 
-int main() {
-    size_t var;
-    Argument arg{"", "", "", var};
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, const char *argv[])
+{
+  try
+  {
+    const std::string kTerminalArgs = util::createStringFromCharPtrArray(argc-1, argv+1);
+  }
+  catch (const std::invalid_argument& invalid_argument)
+  {
+    std::cout << "Invalid argument!" << std::endl;
+  }
+  catch (const std::runtime_error& runtime_error)
+  {
+    std::cout << "Error occurred during runtime!" << runtime_error.what() << std::endl;
+  }
+  catch (const std::exception& exception)
+  {
+    std::cout << "Unspecified error occurred" << std::endl;
+  }
+
+  return 0;
 }
