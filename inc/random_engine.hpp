3ed4ea2d05391b2b75188359f3535cd539ad1ff9
David, Fruehwirth <david.fruehwirth@student.tugraz.at>
2023-06-30 10:42:32 +0200

    Added random engine for uniform distribution

--- inc/random_engine.hpp
+++ inc/random_engine.hpp
@@ -2,8 +2,33 @@
 // Created by david on 30/06/23.
 //
 #pragma once
+#include <random>
+#include <chrono>
 
+template<typename T>
 class AbstractRandomEngine
 {
-  virtual long long getNumber() const = 0;
+protected:
+  virtual T getNumber() = 0;
+};
+
+template<typename T>
+class UniformRandomEngine : public AbstractRandomEngine<T>
+{
+private:
+  std::random_device random_device_;
+  std::mt19937 random_generator_;
+  std::uniform_int_distribution<T> distribution_;
+
+public:
+  UniformRandomEngine() : random_generator_(random_device_()), distribution_()
+  {
+    random_generator_.seed(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+  }
+
+  T getNumber() override
+  {
+    return distribution_(random_generator_);
+  }
+
 };
---
+++ src/random_engine.cpp
@@ -0,0 +1,6 @@
+//
+// Created by david on 30/06/23.
+//
+#include "random_engine.hpp"
+
+
