aec09c2d2b0214ff8cd3390d814c32cbdee90eaf
David, Fruehwirth <david.fruehwirth@student.tugraz.at>
2023-08-05 10:04:19 +0200

    Replaced ternary operator with if

--- src/rand.cpp
+++ src/rand.cpp
@@ -190,7 +190,9 @@ std::string Rand::formatResult(const std::vector<std::string>& output_list) cons
   for (const auto& kString : output_list)
   {
     const bool kLastElement = (--remaining_entries == 0);
-    ss << kString << (kLastElement ? '\0' : kFormatStringDelimiter);
+    ss << kString;
+    if (!kLastElement)
+      ss << kFormatStringDelimiter;
   }
   return ss.str();
 }
